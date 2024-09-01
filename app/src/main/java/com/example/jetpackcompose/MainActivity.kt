be4d016ab272cd12494779443d8da43f265d174c
Shahram Javadi <shahram.developer21@gmail.com>
2024-09-01 13:08:36 +0330

    Work with Text and SpannableText

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -4,19 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,6 +42,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun InitUi() {
 
@@ -40,17 +53,23 @@ fun InitUi() {
                 modifier = Modifier.padding(innerPadding)
             )
             CreateRow()
+            CreateText()
+            Spacer(modifier = Modifier.height(10.dp))
+            CreateSpannableText()
         }
 
     }
 
 }
+
 @Composable
 fun CreateColumn(name: String, modifier: Modifier = Modifier) {
 
-    Column(Modifier.fillMaxWidth(),
+    Column(
+        Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.SpaceBetween,
-        horizontalAlignment = Alignment.CenterHorizontally ) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
             text = "Text 1",
             modifier = modifier
@@ -66,11 +85,62 @@ fun CreateColumn(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun CreateText() {
+
+
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Blue),
+        color = Color.Cyan,
+        fontSize = 36.sp,
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Italic,
+        text = "Testing Text",
+        textDecoration = TextDecoration.LineThrough
+
+    )
+}
+
+@Composable
+fun CreateSpannableText() {
+
+
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Black),
+        color = Color.Cyan,
+        fontSize = 36.sp,
+        fontWeight = FontWeight.Bold,
+        fontStyle = FontStyle.Italic,
+        text = buildAnnotatedString {
+            withStyle(style = SpanStyle(
+                color = Color.Yellow,
+                fontSize = 36.sp,
+                fontWeight = FontWeight.Bold
+            )){
+                append("T")
+            }
+            withStyle(style = SpanStyle(
+                color = Color.Red,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )){
+                append("est")
+            }
+        },
+
+    )
+}
+
+@Composable
 fun CreateRow() {
 
-    Row(Modifier.fillMaxSize(),
+    Row(
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween) {
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
         Text(
             text = "Text 1",
         )
