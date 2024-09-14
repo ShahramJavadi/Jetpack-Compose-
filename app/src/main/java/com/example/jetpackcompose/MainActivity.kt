945a783894e59d54c2a0215acd272bf3fbfd1ac6
Shahram Javadi <shahram.developer21@gmail.com>
2024-09-14 12:17:09 +0330

    working with top action bar

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -27,13 +27,22 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -87,6 +96,7 @@ fun InitUi() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
+            CreateTopAppBarView()
 
             Spacer(modifier = Modifier.height(16.dp))
             GenerateShape(shape = CircleShape, size = 90.dp, color = Color.Red)
@@ -193,6 +203,46 @@ fun CreateListItem(
 
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateTopAppBarView() {
+
+
+    TopAppBar(colors = TopAppBarColors(
+        containerColor = Color.Red,
+        scrolledContainerColor = Color.Red,
+        navigationIconContentColor = Color.Red,
+        actionIconContentColor = Color.Red,
+        titleContentColor = Color.Black), title = {
+
+        Text(text = "AppBar")
+    },
+        modifier = Modifier.background(Color.Black),
+        actions = {
+
+            IconButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+            }
+
+            IconButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = null)
+            }
+
+
+        },
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+            }
+        })
+
+
+}
 
 @Composable
 fun CreateTextField() {
